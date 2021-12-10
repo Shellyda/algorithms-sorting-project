208173352a95b8e95d305aec5d0be773d19298b4
Shellyda <shellydabarbosa@gmail.com>
2021-12-09 22:10:03 -0300

    update(main): isolating five first numbers in a var

--- src/main.py
+++ src/main.py
@@ -27,12 +27,13 @@ def main():
                 """You choosed case 1 - equal a 100 length list of elements!
 A) The first five numbers of the set with NOT sorted elements:"""
             )
-            random_list_elements = get_random_numbers(100)
-            show_five_first_numbers(random_list_elements)
+            random_list = get_random_numbers(100)
+            five_first = random_list[0:5]
+            show_five_first_numbers(random_list)
 
             print("\nB) The first five numbers of sorted list:")
-            sorted_list_elements = SP.Bubble_sort(random_list_elements)
-            show_five_first_numbers(sorted_list_elements)
+            sorted_list = SP.Bubble_sort(random_list)
+            show_five_first_numbers(sorted_list)
         elif case == 2:
             print("teste")
         elif case == 6:
