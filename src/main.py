c7e8af7d0ba096559d9e1ce62f15e66d66fc3755
Denisy Florencio <denisy@hotmail.com>
2021-12-10 14:00:51 -0300

    criei a letra c para todos os números de ranges

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import timeit
+
 from utils.library.sorting_power import Sorting_power as SP
 from utils.functions.generate_random_numbers import get_random_numbers
 from utils.functions.show_numbers import show_five_first_numbers
@@ -27,17 +29,51 @@ def main():
                 """You choosed case 1 - equal a 100 length list of elements!
 A) The first five numbers of the set with NOT sorted elements:"""
             )
-            random_list = get_random_numbers(100)
-            five_first = random_list[0:5]
-            show_five_first_numbers(five_first)
-
+            random_list_elements = get_random_numbers(100)
+            show_five_first_numbers(random_list_elements)
             print("\nB) The first five numbers of sorted list:")
-            sorted_list_elements = SP.Bubble_sort(five_first)
+
+            #Ordenacao e time: bubble sort
+            begin = timeit.default_timer()
+            sorted_list_elements = SP.Bubble_sort(random_list_elements)
+            end = timeit.default_timer() - begin
             show_five_first_numbers(sorted_list_elements)
+
+            print("\nC) Runtime:")
+            print("Tempo de execução Bubble Sort: {} segundos".format(end))
+
         elif case == 2:
-            print("teste")
-        elif case == 6:
-            print("exiting program...")
+            random_list_elements2 = get_random_numbers(1000)
+            #Ordenacao e time: bubble sort
+            begin = timeit.default_timer()
+            sorted_list_elements2 = SP.Bubble_sort(random_list_elements2)
+            end = timeit.default_timer() - begin
+            print("Tempo de execução Bubble Sort: {} segundos".format(end))
+
+        elif case == 3:
+            random_list_elements3 = get_random_numbers(10000)
+            #Ordenacao e time: bubble sort
+            begin = timeit.default_timer()
+            sorted_list_elements3 = SP.Bubble_sort(random_list_elements3)
+            end = timeit.default_timer() - begin
+            print("Tempo de execução Bubble Sort: {} segundos".format(end))
+
+        elif case == 4:
+            random_list_elements4 = get_random_numbers(100000)
+            # Ordenacao e time: bubble sort
+            begin = timeit.default_timer()
+            sorted_list_elements4 = SP.Bubble_sort(random_list_elements4)
+            end = timeit.default_timer() - begin
+            print("Tempo de execução Bubble Sort: {} segundos".format(end))
+
+        elif case == 5:
+            random_list_elements5 = get_random_numbers(1000000)
+            # Ordenacao e time: bubble sort
+            begin = timeit.default_timer()
+            sorted_list_elements5 = SP.Bubble_sort(random_list_elements5)
+            end = timeit.default_timer() - begin
+            print("Tempo de execução Bubble Sort: {} segundos".format(end))
+
         else:
             print("not allowed option! :( Choose other...")
         sleep(1.5)
